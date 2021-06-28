b3be9ba79494c9cc42065bc263f1cafc2f84de07
dineshannayya <dinesh.annayya@gmail.com>
2021-06-28 20:19:49 +0530

    License Text Added

--- verilog/rtl/syntacore/scr1/src/core/pipeline/scr1_pipe_mprf.sv
+++ verilog/rtl/syntacore/scr1/src/core/pipeline/scr1_pipe_mprf.sv
@@ -1,3 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+// SPDX-FileCopyrightText: Syntacore LLC © 2016-2021
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileContributor: Syntacore LLC
+// //////////////////////////////////////////////////////////////////////////
 /// Copyright by Syntacore LLC © 2016-2020. See LICENSE for details
 /// @file       <scr1_pipe_mprf.sv>
 /// @brief      Multi Port Register File (MPRF)
