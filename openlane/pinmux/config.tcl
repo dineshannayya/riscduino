dc63d70039415495f8474b199995f7cd188ee187
dineshannayya <dinesh.annayya@gmail.com>
2021-11-16 08:36:04 +0530

    IP area optimization

--- openlane/pinmux/config.tcl
+++ openlane/pinmux/config.tcl
@@ -62,7 +62,7 @@ set ::env(GND_PIN) [list {vssd1}]
 set ::env(FP_PIN_ORDER_CFG) $::env(DESIGN_DIR)/pin_order.cfg
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 500 500"
+set ::env(DIE_AREA) "0 0 500 400"
 
 
 # If you're going to use multiple power domains, then keep this disabled.
--- openlane/uart_i2cm_usb_spi/config.tcl
+++ openlane/uart_i2cm_usb_spi/config.tcl
@@ -83,7 +83,7 @@ set ::env(GND_PIN) [list {vssd1}]
 
 set ::env(FP_PIN_ORDER_CFG) $::env(DESIGN_DIR)/pin_order.cfg
 set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) [list 0.0 0.0 600.0 700.0]
+set ::env(DIE_AREA) [list 0.0 0.0 550.0 700.0]
 
 
 
